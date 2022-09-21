7e6834a757f60d1a200b74c749ee76ebaf36bd88
Fabian Kalus <kalus.developer@outlook.de>
2022-09-21 14:24:13 +0200

    fixing firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   firebase: {
     projectId: 'ringoffire-5863465',
     appId: '1:319090905772:web:f9efbd00286a87edfbc509',
+    databaseURL: 'https://ringoffire-5863465-default-rtdb.europe-west1.firebasedatabase.app',
     storageBucket: 'ringoffire-5863465.appspot.com',
     locationId: 'europe-west',
     apiKey: 'AIzaSyCPKPfo5dIq7ShFm2uHNO-LpNi9DWtEQzM',
